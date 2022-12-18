2c8f9e5b817aefa472aa8a3c5d65d33ff008c75f
Pereira Júlio <julioocesarr@outlook.fr>
2022-12-18 02:09:17 +0100

    Create sayHello component

---
+++ src/components/SayHelloComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello {{ name }}!</h1>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>
